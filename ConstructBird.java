a09f8d57632360e0c06422a5afd4d8b719f23a71
Dbv2410 <63506793+Dbv2410@users.noreply.github.com>
2020-08-27 19:48:59 +0530

    Add files via upload

---
+++ ConstructBird.java
@@ -0,0 +1,16 @@
+
+public class ConstructBird {
+
+	public static void main(String[] args) {
+		Bird birdObj1 = new Ostrich();
+		birdObj1.eat();
+		
+		Bird birdObj2 = new Parrot();
+		birdObj2.eat();
+		
+		Bird birdObj3 = new Penguin();
+		birdObj3.eat();
+		
+	}
+
+}
